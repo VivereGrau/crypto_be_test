8121d494d1621c18abae8709c01d90f9b3278ddf
VivereGrau <pratompongwiangsamut@gmail.com>
2022-09-22 14:58:14 +0700

    [09-22-2022] add databaseseeder and edit route

--- cypto-app/database/seeders/DatabaseSeeder.php
+++ cypto-app/database/seeders/DatabaseSeeder.php
@@ -3,6 +3,9 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,168 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        //user
+        DB::table('users')->insert([
+            'name'     => "admin",
+            'email'    => 'admin@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'name'     => "customer001",
+            'email'    => 'customer001@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'name'     => "customer002",
+            'email'    => 'customer002@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+        DB::table('users')->insert([
+            'name'     => "customer003",
+            'email'    => 'customer003@gmail.com',
+            'password' => Hash::make('password'),
+        ]);
+
+        //currrency
+        DB::table('currencies')->insert([
+            'currency_name' => "THB",
+            'type'          => 'Fiat',
+        ]);
+        DB::table('currencies')->insert([
+            'currency_name' => "USD",
+            'type'          => 'Fiat',
+        ]);
+        DB::table('currencies')->insert([
+            'currency_name' => "BTC",
+            'type'          => 'Cryptocurrencies',
+        ]);
+        DB::table('currencies')->insert([
+            'currency_name' => "ETH",
+            'type'          => 'Cryptocurrencies',
+        ]);
+        DB::table('currencies')->insert([
+            'currency_name' => "XRP",
+            'type'          => 'Cryptocurrencies',
+        ]);
+        DB::table('currencies')->insert([
+            'currency_name' => "DOGE",
+            'type'          => 'Cryptocurrencies',
+        ]);
+
+        //storages
+        DB::table('storages')->insert([
+            'currency_id'     => 3,
+            'currency_serial' => '1s5d51gw91g62rg1a69rg11hea651heherjkev',
+        ]);
+        DB::table('storages')->insert([
+            'currency_id'     => 4,
+            'currency_serial' => '1s5d51gw91g62rg1a69rg11hea651hehasd1h',
+        ]);
+        DB::table('storages')->insert([
+            'currency_id'     => 5,
+            'currency_serial' => '1s5d51gw91g62jkofngperi9o0g56156651he',
+        ]);
+        DB::table('storages')->insert([
+            'currency_id'     => 6,
+            'currency_serial' => 'mokdf1456odf145rtg591g65hrt5hs2h6s6fg',
+        ]);
+
+        //wallets
+        DB::table('wallets')->insert([
+            'currency_id'     => 3,
+            'user_id'         => 1,
+            'total'           => 1
+        ]);
+        DB::table('wallets')->insert([
+            'currency_id'     => 4,
+            'user_id'         => 2,
+            'total'           => 1
+        ]);
+        DB::table('wallets')->insert([
+            'currency_id'     => 5,
+            'user_id'         => 3,
+            'total'           => 1
+        ]);
+        DB::table('wallets')->insert([
+            'currency_id'     => 6,
+            'user_id'         => 4,
+            'total'           => 1
+        ]);
+
+        //markets
+        DB::table('markets')->insert([
+            'currency_id'     => 3,
+            'user_id'         => 1,
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 16551,
+        ]);
+        DB::table('markets')->insert([
+            'currency_id'     => 4,
+            'user_id'         => 2,
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 1176,
+        ]);
+        DB::table('markets')->insert([
+            'currency_id'     => 5,
+            'user_id'         => 3,
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 0.42,
+        ]);
+        DB::table('markets')->insert([
+            'currency_id'     => 6,
+            'user_id'         => 4,
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 0.04,
+        ]);
+
+        //histories
+        DB::table('histories')->insert([
+            'currency_id'     => 3,
+            'from_user_id'    => 1,
+            'is_in_system'    => 1,
+            'to_user_id'      => 1,
+            'ref_to_user'     => "",
+            'to_address'      => "",
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 16551,
+        ]);
+        DB::table('histories')->insert([
+            'currency_id'     => 4,
+            'from_user_id'    => 2,
+            'is_in_system'    => 1,
+            'to_user_id'      => 2,
+            'ref_to_user'     => "",
+            'to_address'      => "",
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 1176,
+        ]);
+        DB::table('histories')->insert([
+            'currency_id'     => 5,
+            'from_user_id'    => 3,
+            'is_in_system'    => 1,
+            'to_user_id'      => 2,
+            'ref_to_user'     => "",
+            'to_address'      => "",
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 0.42,
+        ]);
+        DB::table('histories')->insert([
+            'currency_id'     => 6,
+            'from_user_id'    => 4,
+            'is_in_system'    => 1,
+            'to_user_id'      => 2,
+            'ref_to_user'     => "",
+            'to_address'      => "",
+            'action'          => "Sell",
+            'total'           => 1,
+            'price'           => 0.04,
+        ]);
     }
 }
--- cypto-app/routes/web.php
+++ cypto-app/routes/web.php
@@ -1,6 +1,10 @@
 <?php
 
 use App\Http\Controllers\CurrenciesController;
+use App\Http\Controllers\HistoriesController;
+use App\Http\Controllers\MarketsController;
+use App\Http\Controllers\StoragesController;
+use App\Http\Controllers\WalletsController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -35,24 +39,24 @@ Route::post('/currency/add', [CurrenciesController::class,'addNewCurrency'])->na
 Route::put('/currency/update', [CurrenciesController::class,'updateCurrency'])->name('updateCurrency');
 Route::delete('/currency/delete', [CurrenciesController::class,'deleteCurrency'])->name('deleteCurrency');
 //market
-Route::get('/market/list', [CurrenciesController::class,'selectMarket'])->name('selectMarket');
-Route::get('/market/byid', [CurrenciesController::class,'getMarketById'])->name('getMarketById');
-Route::post('/market/add', [CurrenciesController::class,'addNewMarket'])->name('addNewMarket');
-Route::delete('/market/delete', [CurrenciesController::class,'deletemarket'])->name('deletemarket');
+Route::get('/market/list', [MarketsController::class,'selectMarket'])->name('selectMarket');
+Route::get('/market/byid', [MarketsController::class,'getMarketById'])->name('getMarketById');
+Route::post('/market/add', [MarketsController::class,'addNewMarket'])->name('addNewMarket');
+Route::delete('/market/delete', [MarketsController::class,'deletemarket'])->name('deletemarket');
 //storages
-Route::get('/currency/list', [CurrenciesController::class,'selectStorage'])->name('selectStorage');
-Route::get('/currency/byid', [CurrenciesController::class,'getStorageById'])->name('getStorageById');
-Route::post('/currency/add', [CurrenciesController::class,'addNewStorage'])->name('addNewStorage');
-Route::put('/currency/update', [CurrenciesController::class,'updateStorage'])->name('updateStorage');
-Route::delete('/currency/delete', [CurrenciesController::class,'deleteStorage'])->name('deleteStorage');
+Route::get('/storages/list', [StoragesController::class,'selectStorage'])->name('selectStorage');
+Route::get('/storages/byid', [StoragesController::class,'getStorageById'])->name('getStorageById');
+Route::post('/storages/add', [StoragesController::class,'addNewStorage'])->name('addNewStorage');
+Route::put('/storages/update', [StoragesController::class,'updateStorage'])->name('updateStorage');
+Route::delete('/storages/delete', [StoragesController::class,'deleteStorage'])->name('deleteStorage');
 //wallets
-Route::get('/currency/list', [CurrenciesController::class,'selectWallet'])->name('selectWallet');
-Route::get('/currency/byid', [CurrenciesController::class,'getWalletById'])->name('getWalletById');
-Route::post('/currency/add', [CurrenciesController::class,'addNewWallet'])->name('addNewWallet');
-Route::put('/currency/update', [CurrenciesController::class,'updateWallet'])->name('updateWallet');
-Route::delete('/currency/delete', [CurrenciesController::class,'deleteWallet'])->name('deleteWallet');
+Route::get('/wallets/list', [WalletsController::class,'selectWallet'])->name('selectWallet');
+Route::get('/wallets/byid', [WalletsController::class,'getWalletById'])->name('getWalletById');
+Route::post('/wallets/add', [WalletsController::class,'addNewWallet'])->name('addNewWallet');
+Route::put('/wallets/update', [WalletsController::class,'updateWallet'])->name('updateWallet');
+Route::delete('/wallets/delete', [WalletsController::class,'deleteWallet'])->name('deleteWallet');
 //histories
-Route::get('/currency/list', [CurrenciesController::class,'selectHistory'])->name('selectHistory');
-Route::get('/currency/byid', [CurrenciesController::class,'getHistoryById'])->name('getHistoryById');
-Route::post('/currency/add', [CurrenciesController::class,'addNewHistory'])->name('addNewHistory');
-Route::delete('/currency/delete', [CurrenciesController::class,'deleteHistory'])->name('deleteHistory');
+Route::get('/histories/list', [HistoriesController::class,'selectHistory'])->name('selectHistory');
+Route::get('/histories/byid', [HistoriesController::class,'getHistoryById'])->name('getHistoryById');
+Route::post('/histories/add', [HistoriesController::class,'addNewHistory'])->name('addNewHistory');
+Route::delete('/histories/delete', [HistoriesController::class,'deleteHistory'])->name('deleteHistory');
